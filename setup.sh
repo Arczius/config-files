312fc1375587dbffa39bd8515bf9f341120e31db
Arczius <damianvaartmans@gmail.com>
2026-03-04 14:53:54 +0100

    automated the installation of Lilex font

--- setup.sh
+++ setup.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
 echo "Starting the setup process"
+
+echo "\n\n"
+
+echo "downloading the Lilex NF"
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Lilex.zip -O Lilex.zip
+
+clear
+
+echo "latest Lilex NF has been downloaded, installing..."
+
+mkdir ~/.local/share/fonts/
+mkdir ~/.local/share/fonts/LilexNerdFont/
+
+unzip -o Lilex.zip -d ~/.local/share/fonts/LilexNerdFont/
+fc-cache
+rm Lilex.zip
